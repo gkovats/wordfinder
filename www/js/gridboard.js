c583d70817c83e9edd73690191e21ba629f772e8
George Kovats <gkovats@webmd.net>
2013-09-09 21:41:59 -0400

    v2

---
+++ www/js/gridboard.js
@@ -0,0 +1,9 @@
+
+/**
+ * This controls the use of the grid-based board that games can be played on
+ * @constructor
+ */
+var GridBoard = function(){
+
+
+};
---
+++ www/js/wordfinder.js
@@ -0,0 +1,19 @@
+
+/**
+ * This is the Singleton, 1 per page, object that tracks and knows what is the Word Finder
+ * @namespace
+ */
+var WF = {
+  version: .1,
+
+
+  grid: new GridBoard(30, 30),
+
+
+
+
+
+
+  // I do this to avoid trailing commas. That's just me.
+  end: 1
+}
