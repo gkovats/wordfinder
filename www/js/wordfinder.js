2ed880fd2b263a324279a01f90a212070a16b68c
George Kovats <george.kovats@tritondigital.com>
2013-09-11 09:10:38 -0400

    Moving the WordFinder JS to private scoped methods. Will expose some functional public methods next.

--- www/js/wordfinder.js
+++ www/js/wordfinder.js
@@ -3,13 +3,14 @@
  * This is the Singleton, 1 per page, object that tracks and knows what is the Word Finder
  * @namespace
  */
-var WF = {
-  version: .1,
+var WordFinder = function(instanceConfig) {
+
+  var version = 1,
 
   /**
-   * selection Object
+   * selection Object - tracks what the user is selecting
    */
-  selection: {
+  selection = {
     y: 0,
     x: 0,
     y2: 0,
@@ -18,110 +19,57 @@ var WF = {
   },
 
   /**
-   * Configuration
+   * Configuration params with defaults
    */
-  config: {
-    height: 20,
-    width: 20,
-    id: 'wordfinder'
+  config = {
+    height: 20, // height in grid squares
+    width: 20, // width in grid squares
+    id: 'wordfinder', // ID for wrapper DOM object
+    words: [] // on init, this gets transferred to the words property
   },
 
   /**
-   * DOM pointers
+   * DOM pointers object
    */
-  dom: {
-    wf: {}
+  dom = {
+    wf: '', // whole wordfinder wrapper
+    board: '', // board Dom element
   },
 
   /**
-   * Grid object
+   * GridBoard instance
    */
-  grid: {},
+  grid = {},
 
   /**
    * Collection of words
    */
-  words: {},
-
-  /**
-   * Initialization - start the game
-   * @param {array} config    Configuration for the WordFinder
-   */
-  init: function(config) {
-
-    var self = this;
-    self.config = $.extend(true, self.config, config);
-
-    self.dom.wf = $('#'+self.config.id);
-    self.dom.board = self.dom.wf.find('.board');
-    self.dom.words = self.dom.wf.find('.words');
-    self.dom.header = self.dom.wf.find('.header');
-
-    self.grid = new GridBoard(self.dom.board, self.config.width, self.config.height);
-
-    self.loadWord('astronaut');
-    self.loadWord('helper');
-    self.loadWord('touchdown');
-    self.loadWord('dietary');
-    self.loadWord('milkshake');
-    self.loadWord('marrow');
-    self.loadWord('crispy');
-    self.loadWord('fluffy');
-    self.loadWord('freedom');
-    self.loadWord('fantasy');
-    self.loadWord('touchback');
-    self.loadWord('tightend');
-    self.loadWord('forward');
-    self.loadWord('safety');
-    self.loadWord('savings');
-    self.loadWord('attached');
-    self.loadWord('strings');
-    self.loadWord('fray');
-    self.loadWord('quarry');
-    self.loadWord('yelling');
-
-
-    console.log(self.words);
-
-    self.grid.render();
-
-    // Apply Event handlers to DOM
-    // Track mouseclicks on board
-    self.dom.board.on('mousedown mouseup', function(event) {
-      self.mouseTrack(event);
-    });
-    // Track clicks on the word list
-    self.dom.words.find('li').on('click', function(e){
-      self.revealWord(this);
-    });
-
-  },
-
+  words = {};
 
   /**
    * Abstraction for grid's loadword
    */
-  loadWord: function(word) {
+  function loadWord (word) {
     var self = this, line;
     word = word.toLowerCase().replace(/\s/,'');
-    line = self.grid.loadWord(word);
+    line = grid.loadWord(word);
 
     if (!line) { return false; }
-    self.words[word] = {
+    words[word] = {
       line: line,
       found: false
     };
     // add word to words board
-    self.dom.words.append('<li data-word="'+word+'">'+word+'</li>');
+    dom.words.append('<li data-word="'+word+'">'+word+'</li>');
     return true;
-  },
+  };
 
   /**
    * Mouse event handlers
    * @param {array} event     Mouse event
    * @return {boolean} Success
    */
-  mouseTrack: function (event) {
+  function mouseTrack (event) {
     var self = this,
       id = event.target.id,
       matches = [];
@@ -131,23 +79,23 @@ var WF = {
     }
 
     if (event.type == 'mousedown') {
-      self.selection.y = parseInt(matches[1]);
-      self.selection.x = parseInt(matches[2]);
-      self.selection.selecting = true;
-    } else if (event.type == 'mouseup' && self.selection.selecting) {
-      self.selection.y2 = parseInt(matches[1]);
-      self.selection.x2 = parseInt(matches[2]);
-      console.log(self.selection);
-      self.selection.selecting = false;
+      selection.y = parseInt(matches[1]);
+      selection.x = parseInt(matches[2]);
+      selection.selecting = true;
+    } else if (event.type == 'mouseup' && selection.selecting) {
+      selection.y2 = parseInt(matches[1]);
+      selection.x2 = parseInt(matches[2]);
+      console.log(selection);
+      selection.selecting = false;
       // if doubleclicked, toss out
-      if (self.selection.x == self.selection.x2 && self.selection.y == self.selection.y2) {
+      if (selection.x == selection.x2 && selection.y == selection.y2) {
         return false;
       }
       // now check word
-      self.checkSelection( self.selection.y, self.selection.x, self.selection.y2, self.selection.x2 );
+      checkSelection( selection.y, selection.x, selection.y2, selection.x2 );
     }
     return true;
-  },
+  }
 
   /**
    * Checks current selection to see if it's on a word
@@ -158,35 +106,35 @@ var WF = {
    * @param {integer} x2   X2 coordinate
    * @returns {boolean} Success
    */
-  checkSelection: function (y, x, y2, x2) {
+  function checkSelection (y, x, y2, x2) {
     var self = this, w, word, line;
 
-    for (word in self.words) {
+    for (word in words) {
       // skip found words
-      if (self.words[word].found) { continue; }
+      if (words[word].found) { continue; }
 
-      line = self.words[word].line;
+      line = words[word].line;
       console.log('Checking word: '+ word, line);
       if ( (x == line.x && y == line.y && x2 == line.x2 && y2 == line.y2) ||
           (x2 == line.x && y2 == line.y && x == line.x2 && y == line.y2) ) {
-        self.dom.header.html('<p>You found ' + word + '!</p>');
-        self.words[word].found = true;
+        dom.header.html('<p>You found ' + word + '!</p>');
+        words[word].found = true;
         // highlight line on board
-        self.grid.highlightLine(line);
+        grid.highlightLine(line);
         // cross off the word list
-        self.dom.words.find('li[data-word='+word+']').addClass('found');
+        dom.words.find('li[data-word='+word+']').addClass('found');
         return true;
       }
     }
 
-    self.dom.header.html('<p class="no">Word not found there.</p>');
+    dom.header.html('<p class="no">Word not found there.</p>');
     return false;
-  },
+  }
 
   /**
    * Reveals a word hidden on the board
    */
-  revealWord: function (wordRef) {
+  function revealWord (wordRef) {
     var self = this, word = '';
     if (typeof wordRef == 'string') {
       word = wordRef;
@@ -195,19 +143,61 @@ var WF = {
     }
     console.log(word);
     // If word doesn't exist or is found, skip
-    if (!self.words[word] || self.words[word].found) {
+    if (!words[word] || words[word].found) {
       return false;
     }
     // mark word found and reveal it
-    self.words[word].found = true;
+    words[word].found = true;
     // highlight line on board
-    self.grid.highlightLine(self.words[word].line);
+    grid.highlightLine(words[word].line);
     // cross off the word list
-    self.dom.words.find('li[data-word='+word+']').addClass('found');
+    dom.words.find('li[data-word='+word+']').addClass('found');
     return true;
-  },
+  }
+
+  // Initialization of WordFinder
+  if (instanceConfig) {
+    config = $.extend(true, config, instanceConfig);
+  }
+
+  // Set Dom pointers
+  dom.wf = $('#'+config.id);
+  dom.board = dom.wf.find('.board');
+  dom.words = dom.wf.find('.words');
+  dom.header = dom.wf.find('.header');
+
+  // Init grid
+  grid = new GridBoard(dom.board, config.width, config.height);
+
+  // Load words
+  for (w in config.words) {
+    loadWord(config.words[w]);
+  }
+  // Render Grid
+  grid.render();
+
+  // Apply Event handlers to DOM
+  // Track mouseclicks on board
+  dom.board.on('mousedown mouseup', function(event) {
+    mouseTrack(event);
+  });
+  // Track clicks on the word list
+  dom.words.find('li').on('click', function(e){
+    revealWord(this);
+  });
 
 
-  // I do this to avoid trailing commas. That's just me.
-  end: 1
+  return {
+
+    /**
+     * Here's where the public methods go
+     */
+    // Clear board method
+    // Load words method
+    // Stop game
+    // Get Score?
+    // Reset / Reshuffle - reload current words
+
+  }
+
 };
